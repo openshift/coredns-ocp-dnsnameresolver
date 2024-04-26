5c0a410ad292436afab73747d00e43c6de4a8cf0
arkadeepsen <arsen@redhat.com>
2024-04-26 23:47:49 +0530

    Add unit tests for dnsnameresolver controller and resolver

--- operator/controller/dnsnameresolver/controller_test.go
+++ operator/controller/dnsnameresolver/controller_test.go
@@ -1,14 +1,28 @@
 package dnsnameresolver
 
 import (
+	"context"
+	"net"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
-	networkv1alpha1 "github.com/openshift/api/network/v1alpha1"
+	"github.com/miekg/dns"
+	ocpnetworkv1alpha1 "github.com/openshift/api/network/v1alpha1"
 	"github.com/stretchr/testify/assert"
+
+	discoveryv1 "k8s.io/api/discovery/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/cache/informertest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func TestIsWildcard(t *testing.T) {
@@ -45,17 +59,17 @@ func TestIsWildcard(t *testing.T) {
 func TestRemovalOfIPsRequired(t *testing.T) {
 	tests := []struct {
 		name           string
-		status         *networkv1alpha1.DNSNameResolverStatus
-		expectedStatus *networkv1alpha1.DNSNameResolverStatus
+		status         *ocpnetworkv1alpha1.DNSNameResolverStatus
+		expectedStatus *ocpnetworkv1alpha1.DNSNameResolverStatus
 		expectedOutput bool
 	}{
 		{
 			name: "None of the TTL of the IP addresses expired",
-			status: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			status: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.1",
 								TTLSeconds:     10,
@@ -70,7 +84,7 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.3",
 								TTLSeconds:     30,
@@ -80,11 +94,11 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 				},
 			},
-			expectedStatus: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			expectedStatus: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:         "1.1.1.1",
 								TTLSeconds: 10,
@@ -97,7 +111,7 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:         "1.1.1.3",
 								TTLSeconds: 30,
@@ -110,11 +124,11 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 		},
 		{
 			name: "The TTL of IP addresses expired, but grace period not over",
-			status: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			status: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.1",
 								TTLSeconds:     10,
@@ -129,7 +143,7 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.3",
 								TTLSeconds:     30,
@@ -139,11 +153,11 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 				},
 			},
-			expectedStatus: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			expectedStatus: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:         "1.1.1.1",
 								TTLSeconds: 10,
@@ -156,7 +170,7 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:         "1.1.1.3",
 								TTLSeconds: 30,
@@ -169,11 +183,11 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 		},
 		{
 			name: "The TTL of IP addresses expired, and grace period of one IP address is also over",
-			status: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			status: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.1",
 								TTLSeconds:     10,
@@ -188,7 +202,7 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.3",
 								TTLSeconds:     30,
@@ -198,11 +212,11 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 				},
 			},
-			expectedStatus: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			expectedStatus: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:         "1.1.1.1",
 								TTLSeconds: 10,
@@ -211,7 +225,7 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:         "1.1.1.3",
 								TTLSeconds: 30,
@@ -229,9 +243,9 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 			actualOutput := removalOfIPsRequired(tc.status)
 			assert.Equal(t, tc.expectedOutput, actualOutput)
 			cmpOpts := []cmp.Option{
-				cmpopts.IgnoreFields(networkv1alpha1.DNSNameResolverResolvedAddress{}, "LastLookupTime"),
+				cmpopts.IgnoreFields(ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{}, "LastLookupTime"),
 				cmpopts.EquateApproxTime(100 * time.Millisecond),
-				cmpopts.SortSlices(func(elem1, elem2 networkv1alpha1.DNSNameResolverResolvedAddress) bool {
+				cmpopts.SortSlices(func(elem1, elem2 ocpnetworkv1alpha1.DNSNameResolverResolvedAddress) bool {
 					return elem1.IP > elem2.IP
 				}),
 			}
@@ -246,17 +260,17 @@ func TestRemovalOfIPsRequired(t *testing.T) {
 func TestReconcileRequired(t *testing.T) {
 	tests := []struct {
 		name                  string
-		status                *networkv1alpha1.DNSNameResolverStatus
+		status                *ocpnetworkv1alpha1.DNSNameResolverStatus
 		expectedTTLExpired    bool
 		expectedRemainingTime time.Duration
 	}{
 		{
 			name: "TTL expired of one IP address of a resolved name",
-			status: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			status: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.1",
 								TTLSeconds:     10,
@@ -266,7 +280,7 @@ func TestReconcileRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.2",
 								TTLSeconds:     4,
@@ -281,11 +295,11 @@ func TestReconcileRequired(t *testing.T) {
 		},
 		{
 			name: "TTL expired of IP addresses of different resolved names, return minimum remaining time till grace period",
-			status: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			status: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.1",
 								TTLSeconds:     10,
@@ -295,7 +309,7 @@ func TestReconcileRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.2",
 								TTLSeconds:     4,
@@ -310,11 +324,11 @@ func TestReconcileRequired(t *testing.T) {
 		},
 		{
 			name: "TTL not expired of any IP address",
-			status: &networkv1alpha1.DNSNameResolverStatus{
-				ResolvedNames: []networkv1alpha1.DNSNameResolverResolvedName{
+			status: &ocpnetworkv1alpha1.DNSNameResolverStatus{
+				ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
 					{
 						DNSName: "*.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.1",
 								TTLSeconds:     10,
@@ -324,7 +338,7 @@ func TestReconcileRequired(t *testing.T) {
 					},
 					{
 						DNSName: "www.example.com.",
-						ResolvedAddresses: []networkv1alpha1.DNSNameResolverResolvedAddress{
+						ResolvedAddresses: []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
 							{
 								IP:             "1.1.1.2",
 								TTLSeconds:     4,
@@ -352,5 +366,203 @@ func TestReconcileRequired(t *testing.T) {
 				}
 			}
 		})
+	}
+}
+
+func Test_Reconcile(t *testing.T) {
+	dnsServerIP := "127.0.0.1"
+	dnsServerPort := "8053"
+	testDNSName := "www.example.com."
+
+	// Start a test dns server.
+	testServer := &dns.Server{Addr: net.JoinHostPort(dnsServerIP, dnsServerPort), Net: "udp"}
+	go testServer.ListenAndServe()
+
+	// Create the handler function for current test case.
+	handleRequest := func(w dns.ResponseWriter, r *dns.Msg) {
+		m := new(dns.Msg)
+		m.SetReply(r)
+		w.WriteMsg(m)
+		if r.Question[0].Name != testDNSName {
+			t.Fatalf("Unexpected DNS name lookup request. Expected: %s, Actual, %s", testDNSName, r.Question[0].Name)
+		}
+	}
+	// Register the handler function.
+	dns.HandleFunc(".", handleRequest)
+
+	// Create endpoint slice list containing the dnsServerIP.
+	epSliceList := &discoveryv1.EndpointSliceList{
+		Items: []discoveryv1.EndpointSlice{
+			{
+				ObjectMeta: v1.ObjectMeta{
+					Name: "dns-default",
+				},
+				Endpoints: []discoveryv1.Endpoint{
+					{
+						Addresses: []string{dnsServerIP},
+					},
+				},
+			},
+		},
+	}
+
+	// Add the  endpoint slice list and the scheme for the DNSNameResolver
+	// resource to the fake client and use the fake client to initialize
+	// the fake cache.
+	scheme := runtime.NewScheme()
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(ocpnetworkv1alpha1.Install(scheme))
+	fakeClient := fake.NewClientBuilder().WithLists(epSliceList).WithScheme(scheme).Build()
+	cache := &fakeCache{
+		FakeInformers: &informertest.FakeInformers{},
+		Reader:        fakeClient,
+	}
+
+	// Initialize the resolver using the fake cache.
+	resolver := NewResolver(cache, "")
+	// Use the fake cache, fake client and the resolver to initialize the
+	// reconciler.
+	rec := reconciler{
+		dnsNameResolverCache: cache,
+		client:               fakeClient,
+		resolver:             resolver,
+	}
+
+	// Create a DNSNameResolver object without any status.
+	dnsNameResolver := &ocpnetworkv1alpha1.DNSNameResolver{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "regular",
+			Namespace: "namespace1",
+		},
+		Spec: ocpnetworkv1alpha1.DNSNameResolverSpec{
+			Name: ocpnetworkv1alpha1.DNSName(testDNSName),
+		},
+	}
+	err := fakeClient.Create(context.Background(), dnsNameResolver, &client.CreateOptions{})
+	if err != nil {
+		t.Fatalf("Unexpected error while creating DNSNameResolver object: %v", err)
+	}
+
+	// Recocile the create event of the DNSNameResolver object.
+	_, err = rec.Reconcile(context.Background(), reconcile.Request{NamespacedName: types.NamespacedName{
+		Name:      dnsNameResolver.Name,
+		Namespace: dnsNameResolver.Namespace,
+	}})
+	if err != nil {
+		t.Fatalf("Unexpected error while reconciling the DNSNameResolver object create event: %v", err)
+	}
+
+	// As the DNSNameResolver object did not have any status, the nextLookupTime should match the
+	// current time + defaultMaxTTL. Also, numIPs should be 0.
+	var (
+		dnsName        string
+		nextLookupTime time.Time
+		numIPs         int
+		exists         bool
+	)
+	err = wait.PollUntilContextTimeout(context.Background(), 100*time.Millisecond, 1*time.Second, true, func(ctx context.Context) (done bool, err error) {
+		dnsName, nextLookupTime, numIPs, exists = resolver.getNextDNSNameDetails()
+		if !exists {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		t.Fatal("No next DNS name to lookup found")
+	}
+	if dnsName != testDNSName {
+		t.Fatalf("Unexpected next DNS name to lookup. Expected: %s, Actual: %s", dnsName, testDNSName)
+	}
+	if numIPs != 0 {
+		t.Fatalf("Expected num IPs to 0, but got %d", numIPs)
+	}
+	cmpOpts := cmpopts.EquateApproxTime(1 * time.Second)
+	diff := cmp.Diff(time.Now().Add(defaultMaxTTL), nextLookupTime, cmpOpts)
+	if diff != "" {
+		t.Fatalf("unexpected next lookuptime (-want +got): %s\n", diff)
+	}
+
+	// Update the DNSNameResolver object to the details of the corresponding DNS name's
+	// resolved name to the status.
+	var ttlSeconds int32 = 600
+	addresses := []string{"1.1.1.1", "2.2.2.2", "3.3.3.3"}
+	resolvedAddresses := []ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{}
+	for _, address := range addresses {
+		resolvedAddresses = append(resolvedAddresses, ocpnetworkv1alpha1.DNSNameResolverResolvedAddress{
+			IP:             address,
+			TTLSeconds:     ttlSeconds,
+			LastLookupTime: &v1.Time{Time: time.Now()},
+		})
+	}
+	dnsNameResolver.Status = ocpnetworkv1alpha1.DNSNameResolverStatus{
+		ResolvedNames: []ocpnetworkv1alpha1.DNSNameResolverResolvedName{
+			{
+				DNSName:           ocpnetworkv1alpha1.DNSName(testDNSName),
+				ResolvedAddresses: resolvedAddresses,
+			},
+		},
+	}
+	err = fakeClient.Update(context.Background(), dnsNameResolver, &client.UpdateOptions{})
+	if err != nil {
+		t.Fatalf("Unexpected error while creating DNSNameResolver object: %v", err)
+	}
+
+	// Recocile the update event of the DNSNameResolver object.
+	_, err = rec.Reconcile(context.Background(), reconcile.Request{NamespacedName: types.NamespacedName{
+		Name:      dnsNameResolver.Name,
+		Namespace: dnsNameResolver.Namespace,
+	}})
+	if err != nil {
+		t.Fatalf("Unexpected error while reconciling the DNSNameResolver object update event: %v", err)
+	}
+
+	// As the DNSNameResolver object's status is updated with the details of the corresponding DNS name's
+	// resolved name, the nextLookupTime should match the current time + TTL. Also, numIPs should be 1.
+	err = wait.PollUntilContextTimeout(context.Background(), 100*time.Millisecond, 1*time.Second, true, func(ctx context.Context) (done bool, err error) {
+		dnsName, nextLookupTime, numIPs, exists = resolver.getNextDNSNameDetails()
+		if !exists {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		t.Fatal("No next DNS name to lookup found")
+	}
+	if dnsName != testDNSName {
+		t.Fatalf("Unexpected next DNS name to lookup. Expected: %s, Actual: %s", dnsName, testDNSName)
+	}
+	if numIPs != len(addresses) {
+		t.Fatalf("Expected num IPs to 1, but got %d", numIPs)
+	}
+	diff = cmp.Diff(time.Now().Add(time.Duration(ttlSeconds)*time.Second), nextLookupTime, cmpOpts)
+	if diff != "" {
+		t.Fatalf("unexpected next lookuptime (-expected +actual): %s\n", diff)
+	}
+
+	// Delete a DNSNameResolver object.
+	err = fakeClient.Delete(context.Background(), dnsNameResolver, &client.DeleteOptions{})
+	if err != nil {
+		t.Fatalf("Unexpected error while deleting the DNSNameResolver object: %v", err)
+	}
+
+	// Recocile the delete event of the DNSNameResolver object.
+	_, err = rec.Reconcile(context.Background(), reconcile.Request{NamespacedName: types.NamespacedName{
+		Name:      dnsNameResolver.Name,
+		Namespace: dnsNameResolver.Namespace,
+	}})
+	if err != nil {
+		t.Fatalf("Unexpected error while getting random CoreDNS pod IPs: %v", err)
+	}
+
+	// As the DNSNameResolver object is deleted the next dns name should not exist.
+	err = wait.PollUntilContextTimeout(context.Background(), 100*time.Millisecond, 1*time.Second, true, func(ctx context.Context) (done bool, err error) {
+		dnsName, _, _, exists = resolver.getNextDNSNameDetails()
+		if exists {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		t.Fatalf("Unexpected next DNS name found: %s", dnsName)
 	}
 }
